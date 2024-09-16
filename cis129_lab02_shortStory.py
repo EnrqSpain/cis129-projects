30e345a01da4a9ca165e97af354fc0cd993c2d36
Nicolas Spain <147004659+EnrqSpain@users.noreply.github.com>
2024-09-16 07:39:17 -0700

    Update Story in file

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+# Author: Nicolas Spain
+# Python Lab 2
+# Short Story Print
+
+
+print("He woke up in an unfamiliar room")
+print("It was dark, but he could still make out a few words")
+print("He squinted his eyes")
+print("It read TOO LATE")
